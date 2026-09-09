7851d103dfbc1427f9c4af47ae7b707efa8b7c42
kangaroo-tech-300 <kangarootechnologies300@gmail.com>
2026-09-09 12:32:38 +0100

    fix: correct ThemeProvider import to @react-navigation/native

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ import { MessageSyncProvider } from '@/components/messages/message-sync-provider
 import { SplashScreen } from '@/components/splash/splash-screen';
 import { DesignColors } from '@/constants/design';
 
-import { DarkTheme, ThemeProvider } from 'expo-router/react-navigation';
+import { DarkTheme, ThemeProvider } from '@react-navigation/native';
 
 const customTheme = {
   ...DarkTheme,
